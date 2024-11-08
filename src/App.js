5b96798d4b64827161f20377c7ef767502336e41
Nicko-rgb <mancillanixon7@gmail.com>
2024-11-08 07:17:37 -0500

    miapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router >
+            <h2>HELLO WORD</h2>     
+        </Router>
+    );
+}
+
+export default App;
